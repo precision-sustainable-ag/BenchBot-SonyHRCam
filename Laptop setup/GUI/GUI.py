78eb14533b97ab2697195fb115fa2a33cfb37b38
Priya Jakhar <80418210+priyajakhar@users.noreply.github.com>
2022-07-14 18:42:40 -0400

    GUI updates
    includes validations, image capture method change, other smaller modifications

--- Laptop setup/GUI/GUI.py
+++ Laptop setup/GUI/GUI.py
@@ -1,36 +1,30 @@
-# Before getting started, some things that you may need to change on the script
-# line 25 change your state to 'MD', 'NC' or 'TX'
-# Function def filerename(pot), change if fname.startswith('MDX'):, ('MDX') to 'NCX' ot 'TXX' 
-
-state = 'MD' # for now change this to have files renamed with the state you are in
 lenRobot = 133  # Length of the robot (between front and back sensors) measured in cm
-widRobot =215 # Width of the robot (distance between two front wheels) in cm
+widRobot = 215 # Width of the robot (distance between two front wheels) in cm
 pi_username = 'benchbot' # if you haven't changed your pi's name the default username is 'pi'
 pi_password = 'bb-semi-field' # if you haven't changed your pi's password, the default is 'raspberrypi'
 distTravel = 300
-wheelMotors = [2, 3] # if BB moving backwards change the motors order to [3,2]
+wheelMotors = [2,3] # if BB moving backwards change the motors order to [3,2]
 
 import os, sys, time, pandas as pd, openpyxl, threading
+import string, shutil, datetime, paramiko, socket, numpy as np
+from datetime import date
 sys.path.append("..")
 from MachineMotion import *
 from PyQt6 import QtWidgets
 from PyQt6.QtCore import QTimer
 from PyQt6.QtWidgets import *
-import paramiko
-import socket
-import numpy as np
-from datetime import date
+from PyQt6.QtGui import QIntValidator
 
 HOST = "192.168.7.3"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
 global stop_exec
 global process_complete
-#global state
+global state
 
 ############################### Ultrasonic sensors and laptop-pi communication functions ###############################
 
-# Helper function for getting disntance measurements from sensor
+# Helper function for getting distance measurements from sensor
 def getDistances(s, offsets):
     # Number of queries of sensor measurements
     N = 3
@@ -110,16 +104,23 @@ class mainWindow(QWidget):
         qbtn.clicked.connect(QApplication.instance().quit)
         qbtn.resize(qbtn.sizeHint())
         qbtn.move(330, 260)
+        l3 = QLabel('State')
+        self.statebox = QComboBox()
+        self.statebox.addItem('NC')
+        self.statebox.addItem('TX')
+        self.statebox.addItem('MD')
         
         grid = QGridLayout()
         grid.setSpacing(10)
         
         grid.addWidget(QLabel("            "), 0, 0)
-        grid.addWidget(l1, 0, 2)
-        grid.addWidget(l2, 2, 1, 1, 3)
-        grid.addWidget(btnyes, 3, 1)
-        grid.addWidget(btnno, 3, 3)
-        grid.addWidget(qbtn, 5, 4)
+        grid.addWidget(l1, 1, 3)
+        grid.addWidget(l2, 2, 2, 1, 3)
+        grid.addWidget(l3, 0, 4)
+        grid.addWidget(self.statebox, 0, 5)
+        grid.addWidget(btnyes, 3, 2)
+        grid.addWidget(btnno, 3, 4)
+        grid.addWidget(qbtn, 5, 5)
 
         self.setLayout(grid)
         
@@ -128,9 +129,13 @@ class mainWindow(QWidget):
         self.show()
 
     def optyes(self):
+        global state
+        state = self.statebox.currentText()
         mwin.setCurrentIndex(mwin.currentIndex()+1)
     
     def optno(self):
+        global state
+        state = self.statebox.currentText()
         page2 = Page2()
         mwin.addWidget(page2)
         mwin.setCurrentIndex(mwin.currentIndex()+2)
@@ -153,7 +158,7 @@ class Page1(QWidget):
         self.l1 = QLabel("Name of Species")
         self.l2 = QLabel("Row Numbers")
         self.btn1 = QPushButton('UPDATE', self)
-        self.btn1.clicked.connect(self.updateExcel)
+        self.btn1.clicked.connect(self.checkCells)
         self.btn1.setEnabled(False)
 
         self.layout = QGridLayout()
@@ -180,7 +185,41 @@ class Page1(QWidget):
 
         self.btn1.setEnabled(True)
         self.earlier_cnt = count
-        # issues with too much variance
+		
+    def checkCells(self):
+        ## logic for checking contents of the fields before proceeding
+        err_code = 0
+        count = int(self.total_species.currentText())
+        allowed_chars1=['1','2','3','4','5','6','7','8','9','0',',','-']
+        allowed_chars2=list(string.ascii_letters)
+        allowed_chars2.append(' ')
+
+        for c in range(0,count):
+            input_text1 = self.row_list[c].text()
+            input_text2 = self.species_list[c].text()
+            if input_text1=='' or input_text2=='':
+                err_code = 1
+            elif any(x not in allowed_chars1 for x in input_text1):
+                err_code = 2
+            elif any(x not in allowed_chars2 for x in input_text2):
+                err_code = 3
+            else:
+                continue
+        
+        dlg = QMessageBox(self)
+        dlg.setWindowTitle("Error")
+        if err_code!=0:
+            if err_code == 1:
+                dlg.setText("Make sure all fields have entries")
+            elif err_code==2:
+                dlg.setText("Enter row data in following format: '2' or '1-3' or '1,5,8' or '2-3,5'")
+            else:
+                dlg.setText("Make sure Species names only contain alphabets")
+            dlg.setStandardButtons(QMessageBox.StandardButton.Close)
+            if dlg.exec() == QMessageBox.StandardButton.Close:      
+                dlg.done(1)
+        else:
+            self.updateExcel()
 
     def updateExcel(self):
         count = int(self.total_species.currentText())
@@ -189,13 +228,32 @@ class Page1(QWidget):
         for c in range(0,count):
             sheet.cell(row = c+2, column = 1).value = self.species_list[c].text()
             sheet.cell(row = c+2, column = 2).value = self.row_list[c].text()
+        for c in range(count,9):
+            sheet.cell(row = c+2, column = 1).value = ''
+            sheet.cell(row = c+2, column = 2).value = ''
+            sheet.cell(row = c+2, column = 3).value = ''
         wb.save('SpeciesSheet.xlsx')
         os.system("python sheetupdateSpecies.py")
         #time.sleep(0.1)
+        threading.Thread(target=backupSheet).start()
         
         page2 = Page2()
         mwin.addWidget(page2)
         mwin.setCurrentIndex(mwin.currentIndex()+1)
+        
+def backupSheet():
+    # create a copy of the sheet
+    shutil.copy("SpeciesSheet.xlsx", "new.xlsx")
+    
+    # delete the images column
+    wb = openpyxl.load_workbook('new.xlsx')
+    sheet = wb.active
+    sheet.delete_cols(3, 1)
+    wb.save('new.xlsx')
+
+    # include date in file name
+    today = datetime.datetime.today().strftime ('%d-%b-%Y')
+    os.rename(r'new.xlsx',r'SpeciesSheet_' + state + str(today) + '.xlsx')
 
 
 class Page2(QWidget):
@@ -205,7 +263,8 @@ class Page2(QWidget):
         l1 = QLabel("Species")
         l2 = QLabel("Number of Images to Capture")
         btn = QPushButton('NEXT', self)
-        btn.clicked.connect(self.confirmmsg)
+        btn.clicked.connect(self.checkContent)
+        self.onlyInt = QIntValidator(0,8,self)
         
         self.layout = QGridLayout()
         self.layout.addWidget(QLabel("            "), 0, 0)
@@ -220,12 +279,32 @@ class Page2(QWidget):
         for sname in species_names:
             self.layout.addWidget(QLabel(sname[0]), i+2, 1)
             self.snaps[i] = QLineEdit()
+            self.snaps[i].setValidator(self.onlyInt)
             self.layout.addWidget(self.snaps[i], i+2, 2)
             i += 1
 
         self.setLayout(self.layout)
         self.setGeometry(100, 100, 600, 500)
         self.setWindowTitle('BenchBot')
+		
+    def checkContent(self):
+        err_code = 0
+        for c in range(0,len(self.snaps)):
+            input_text = self.snaps[c].text()
+            if input_text=='':
+                err_code = 1
+            else:
+                continue
+
+        if err_code!=0:
+            dlg = QMessageBox(self)
+            dlg.setWindowTitle("Error")
+            dlg.setText("Make sure all fields have entries")
+            dlg.setStandardButtons(QMessageBox.StandardButton.Close)
+            if dlg.exec() == QMessageBox.StandardButton.Close:      
+                dlg.done(1)
+        else:
+            self.confirmmsg()
 
     def confirmmsg(self):
         dlg = QMessageBox(self)
@@ -254,7 +333,8 @@ class Page3(QWidget):
     def __init__(self):
         super().__init__()
         
-        self.mm = MachineMotion(DEFAULT_IP_ADDRESS.usb_windows)
+        # Initialize the machine motion object
+        self.mm = MachineMotion(DEFAULT_IP_ADDRESS.usb_windows)        
         self.camMotor = 1
 
         self.l1 = QLabel("Image Acquisition Process Started")
@@ -285,39 +365,34 @@ class Page3(QWidget):
         stop_exec = False
         global process_complete
         process_complete = False
+
         self.mm.configAxis(self.camMotor, MICRO_STEPS.ustep_8, MECH_GAIN.ballscrew_10mm_turn)
         self.mm.configAxisDirection(self.camMotor, DIRECTION.POSITIVE)
 
-        #wheelMotors = [2,3]
-        directions = ["negative","positive"]
+        directions = ["negative", "positive"]
         for axis in wheelMotors:
             self.mm.configAxis(axis, MICRO_STEPS.ustep_8, MECH_GAIN.enclosed_timing_belt_mm_turn)
             self.mm.configAxisDirection(axis, directions[axis-2])
         self.mm.emitAcceleration(50)
         self.mm.emitSpeed(80)
         path = os.getcwd()+'\\RemoteCli\\RemoteCli.exe'
-        
+
         df  = pd.read_excel('ImagesSheet.xlsx')
         colvalues = df[['ImagesCount']].values
-        i = 0
-        pots = [None] * colvalues.size
+        pots = []
         for num in colvalues:
-            pots[i] = num[0]
-            i = i+1
+            pots.append(num[0])
         
         self.mm.releaseEstop()
         self.mm.resetSystem()
-
         # Create new directory where today's images will be saved.
         dirName=f'{state}_{date.today()}'
-        if dirName in os.listdir():
-            os.chdir(f'{os.getcwd()}\{dirName}')
-            print('Folder already created')
-        else:
+        if dirName not in os.listdir():
             os.mkdir(f'{os.getcwd()}\{dirName}')
-            os.chdir(f'{os.getcwd()}\{dirName}')
+        os.chdir(f'{os.getcwd()}\{dirName}')
             
-        row = 1
+        direction = True
+        
         for j in pots:
             if j==0:
                 if stop_exec:
@@ -342,7 +417,6 @@ class Page3(QWidget):
 
                 self.mm.moveRelativeCombined(wheelMotors, [distTravel, distTravel])
                 self.mm.waitForMotionCompletion()
-                row += 1
                 continue
             else:
                 dist_corrected = getDistances(s, offsets)
@@ -362,15 +436,23 @@ class Page3(QWidget):
                 elif ang < -0.5:
                     self.mm.moveRelative(wheelMotors[0], d_correction_mm)
                     self.mm.waitForMotionCompletion()
+                
+                ct = int(800/(j-1))
                     
-                c = int(1000/(j-1)) #total distance between home and end sensor
+                if direction:
+                    c = ct
+                    direction = False
+                else:
+                    c = -ct
+                    direction = True
+
                 for i in range(1,j):
                     if stop_exec:
                         break
                     #Trigger capture of image
                     os.startfile(path)
                     time.sleep(15)
-                    threading.Thread(target=filerename(row)).start()
+                    threading.Thread(target=filerename()).start()
                     #Move camera plate to next point
                     self.mm.moveRelative(self.camMotor, c)
                     self.mm.waitForMotionCompletion()
@@ -379,14 +461,10 @@ class Page3(QWidget):
                 #Trigger image capture at last point
                 os.startfile(path)
                 time.sleep(15)
-                threading.Thread(target=filerename(row)).start()
-                #move camera plate to start location
-                self.mm.moveToHome(self.camMotor)
-                self.mm.waitForMotionCompletion()
-
+                threading.Thread(target=filerename()).start()
+                
                 self.mm.moveRelativeCombined(wheelMotors, [distTravel, distTravel])
                 self.mm.waitForMotionCompletion()
-                row += 1
 
         if stop_exec:
             self.stop
@@ -427,18 +505,16 @@ class Page3(QWidget):
             elapsed_min = int(elapsed_time/60) - 60*elapsed_hr
             self.l2.setText('     Time Elapsed: '+str(elapsed_hr)+ ' hrs '+str(elapsed_min)+ ' mins')
 
-
-def filerename(pot):
+def filerename():
     global state
-    #time.sleep(2)
-    time.sleep(3) #changed to 10 to test since file renaming not working
+    time.sleep(2)
     t = str(int(time.time()))
     for fname in os.listdir('.'):
-        if fname.startswith('MDX'):
+        if fname.startswith(state+'X'):
             if fname.endswith('.JPG'):
-                dst = f"{state}_Row-{str(pot)}_{t}.JPG" 
+                dst = f"{state}_{t}.JPG" 
             elif fname.endswith('.ARW'):
-                dst = f"{state}_Row-{str(pot)}_{t}.ARW"
+                dst = f"{state}_{t}.ARW"
         else:
             continue
         os.rename(f"{fname}", f"{dst}")
